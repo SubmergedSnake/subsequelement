f72622ac07060aed8458129ccf3a555b157c14eb
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-07-30 22:59:05 +0300

    Happy path for finding elements in grid & uneven grid ok

--- tests/unit/finding/findClosestElement.test.ts
+++ tests/unit/finding/findClosestElement.test.ts
@@ -1,20 +1,120 @@
 import { findClosestElement } from "../../../src/finding/findClosestElement";
 import { Direction, Strategy } from "../../../src/types";
 import { simplegrid3by3 } from "../mockLayouts/simpleGrid3by3";
+import { unevenGrid3by3 } from "../mockLayouts/unevenGrid3by3";
 
 describe('findClosestElement', () => {
 
-	describe('to the right', () => {
+	describe('simplegrid3by3', () => {
 
-		test('returns element six for element nine', () => {
+		describe('to the right', () => {
 
-			const elementNine = simplegrid3by3[8];
-			const firstEightElements = simplegrid3by3.slice(0, 8);
+			test('returns element five for element two', () => {
 
-			const element = findClosestElement(elementNine, firstEightElements, Direction.LEFT, Strategy.STRICT)
-			expect(element.id).toEqual('six')
+				const elements = [...simplegrid3by3]
+				const [elementTwo] = elements.splice(1, 1);
 
+				const element = findClosestElement(elementTwo, elements, Direction.RIGHT, Strategy.STRICT)
+				expect(element.id).toEqual('five')
+
+			})
 		})
+
+		describe('to the left', () => {
+
+			test('returns element four for element seven', () => {
+
+				const elements = [...simplegrid3by3]
+				const [elementSeven] = elements.splice(6, 1);
+
+				const element = findClosestElement(elementSeven, elements, Direction.LEFT, Strategy.STRICT)
+				expect(element.id).toEqual('four')
+
+			})
+		})
+
+		describe('below', () => {
+
+			test('returns element five for element four', () => {
+
+				const elements = [...simplegrid3by3]
+				const [elementFour] = elements.splice(3, 1);
+
+				const element = findClosestElement(elementFour, elements, Direction.DOWN, Strategy.STRICT)
+				expect(element.id).toEqual('five')
+
+			})
+		})
+
+		describe('above', () => {
+
+			test('returns element eight for element nine', () => {
+
+				const elements = [...simplegrid3by3]
+				const [elementNine] = elements.splice(8, 1);
+
+				const element = findClosestElement(elementNine, elements, Direction.UP, Strategy.STRICT)
+				expect(element.id).toEqual('eight')
+
+			})
+		})
+
+	})
+
+	describe('unevengrid3by3', () => {
+
+		describe('to the right', () => {
+
+			test('returns element five for element two', () => {
+
+				const elements = [...unevenGrid3by3]
+				const [elementTwo] = elements.splice(1, 1);
+
+				const element = findClosestElement(elementTwo, elements, Direction.RIGHT, Strategy.STRICT)
+				expect(element.id).toEqual('five')
+
+			})
+		})
+
+		describe('to the left', () => {
+
+			test('returns element four for element seven', () => {
+
+				const elements = [...unevenGrid3by3]
+				const [elementSeven] = elements.splice(6, 1);
+
+				const element = findClosestElement(elementSeven, elements, Direction.LEFT, Strategy.STRICT)
+				expect(element.id).toEqual('four')
+
+			})
+		})
+
+		describe('below', () => {
+
+			test('returns element three for element four', () => {
+
+				const elements = [...unevenGrid3by3]
+				const [elementFour] = elements.splice(3, 1);
+
+				const element = findClosestElement(elementFour, elements, Direction.DOWN, Strategy.STRICT)
+				expect(element.id).toEqual('three')
+
+			})
+		})
+
+		describe('above', () => {
+
+			test('returns element seven for element eight', () => {
+
+				const elements = [...unevenGrid3by3]
+				const [elementEight] = elements.splice(7, 1);
+
+				const element = findClosestElement(elementEight, elements, Direction.UP, Strategy.STRICT)
+				expect(element.id).toEqual('seven')
+
+			})
+		})
+
 	})
 
 })
