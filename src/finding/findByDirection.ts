f6e4e6eec3b8734fdd36563e3732db632eaecc55
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-08-01 15:33:37 +0300

    Rename variables in findByDirection to better convey meaning

--- src/finding/findByDirection.ts
+++ src/finding/findByDirection.ts
@@ -2,25 +2,25 @@ import { Direction, ImplementsGetBoundingClientRect } from "../types"
 
 type hasRect = ImplementsGetBoundingClientRect
 
-const north = (se: hasRect) => (oe: hasRect) => oe.getBoundingClientRect().bottom <= se.getBoundingClientRect().top
-const east = (se: hasRect) => (oe: hasRect) => oe.getBoundingClientRect().left >= se.getBoundingClientRect().right
-const south = (se: hasRect) => (oe: hasRect) => oe.getBoundingClientRect().top >= se.getBoundingClientRect().bottom
-const west = (se: hasRect) => (oe: hasRect) => oe.getBoundingClientRect().right <= se.getBoundingClientRect().left
+const northernElements = (se: hasRect) => (oe: hasRect) => oe.getBoundingClientRect().bottom <= se.getBoundingClientRect().top
+const easternElements = (se: hasRect) => (oe: hasRect) => oe.getBoundingClientRect().left >= se.getBoundingClientRect().right
+const southernElements = (se: hasRect) => (oe: hasRect) => oe.getBoundingClientRect().top >= se.getBoundingClientRect().bottom
+const westernElements = (se: hasRect) => (oe: hasRect) => oe.getBoundingClientRect().right <= se.getBoundingClientRect().left
 
-const directionFilters = {
-	"NORTH": (se: hasRect, oElements: hasRect[]) => oElements.filter(north(se)),
-	"NORTHEAST": (se: hasRect, oElements: hasRect[]) => oElements.filter(north(se)).filter(east(se)),
-	"EAST": (se: hasRect, oElements: hasRect[]) => oElements.filter(east(se)),
-	"SOUTHEAST": (se: hasRect, oElements: hasRect[]) => oElements.filter(south(se)).filter(east(se)),
-	"SOUTH": (se: hasRect, oElements: hasRect[]) => oElements.filter(south(se)),
-	"SOUTHWEST": (se: hasRect, oElements: hasRect[]) => oElements.filter(south(se)).filter(west(se)),
-	"WEST": (se: hasRect, oElements: hasRect[]) => oElements.filter(west(se)),
-	"NORTHWEST": (se: hasRect, oElements: hasRect[]) => oElements.filter(north(se)).filter(west(se)),
+const findFunctions = {
+	"NORTH": (se: hasRect, oElements: hasRect[]) => oElements.filter(northernElements(se)),
+	"NORTHEAST": (se: hasRect, oElements: hasRect[]) => oElements.filter(northernElements(se)).filter(easternElements(se)),
+	"EAST": (se: hasRect, oElements: hasRect[]) => oElements.filter(easternElements(se)),
+	"SOUTHEAST": (se: hasRect, oElements: hasRect[]) => oElements.filter(southernElements(se)).filter(easternElements(se)),
+	"SOUTH": (se: hasRect, oElements: hasRect[]) => oElements.filter(southernElements(se)),
+	"SOUTHWEST": (se: hasRect, oElements: hasRect[]) => oElements.filter(southernElements(se)).filter(westernElements(se)),
+	"WEST": (se: hasRect, oElements: hasRect[]) => oElements.filter(westernElements(se)),
+	"NORTHWEST": (se: hasRect, oElements: hasRect[]) => oElements.filter(northernElements(se)).filter(westernElements(se)),
 } satisfies { [key in Direction]: (startingElement: hasRect, otherElements: hasRect[]) => hasRect[] }
 
 export const findInDirection = (startingElement: hasRect, otherElements: hasRect[], direction: Direction): hasRect[] => {
-	const directionFunc = directionFilters[direction]
-	const elements = directionFunc(startingElement, otherElements)
+	const findFunc = findFunctions[direction]
+	const elements = findFunc(startingElement, otherElements)
 	return elements
 }
 
