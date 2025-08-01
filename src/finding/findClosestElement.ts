eba67726c4c3ba630e145ea08fede8bfa7eaf6ad
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-08-01 16:07:11 +0300

    Strict mode for diagonal finding needs work next

--- src/finding/findClosestElement.ts
+++ src/finding/findClosestElement.ts
@@ -16,25 +16,12 @@ export const findClosestElement =
 			validElements = otherElements
 		}
 
-		switch (direction) {
-			case Direction.NORTH:
-				validElements = findInDirection(startingElement, validElements, Direction.NORTH)
-				break;
-			case Direction.SOUTH:
-				validElements = findInDirection(startingElement, validElements, Direction.SOUTH)
-				break;
-			case Direction.EAST:
-				validElements = findInDirection(startingElement, validElements, Direction.EAST)
-				break;
-			case Direction.WEST:
-				validElements = findInDirection(startingElement, validElements, Direction.WEST)
-				break;
-		}
+		const elementsInDirection = findInDirection(startingElement, validElements, direction)
 
-		let elementsWithDistances: (ImplementsGetBoundingClientRect & ClosestPoint)[] = validElements.map(e => ({ ...e, ...findClosestPointRelativeToStartingElement(startingElement, e) }))
+		let elementsWithDistances: (ImplementsGetBoundingClientRect & ClosestPoint)[] = elementsInDirection.map(e => ({ ...e, ...findClosestPointRelativeToStartingElement(startingElement, e) }))
 		if (elementsWithDistances.length === 0) {
-			const phantomElement = getNewStartingPoint(startingElement, validElements, direction)
-			elementsWithDistances = validElements.map(e => ({ ...e, ...findClosestPointRelativeToStartingElement(phantomElement, e) }))
+			const phantomElement = getNewStartingPoint(startingElement, elementsInDirection, direction)
+			elementsWithDistances = elementsInDirection.map(e => ({ ...e, ...findClosestPointRelativeToStartingElement(phantomElement, e) }))
 		}
 		return elementsWithDistances.reduce((acc, curr) => curr.distance < acc.distance ? curr : acc)
 
--- tests/integration/findClosestElement.test.ts
+++ tests/integration/findClosestElement.test.ts
@@ -7,55 +7,56 @@ describe('findClosestElement', () => {
 
 	describe('simplegrid3by3', () => {
 
-		describe('to the right', () => {
-
-			test('returns element five for element two', () => {
-
-				const elements = [...simplegrid3by3]
-				const [elementTwo] = elements.splice(1, 1);
-
-				const element = findClosestElement(elementTwo, elements, Direction.EAST, Strategy.STRICT)
-				expect(element.id).toEqual('five')
-
-			})
-		})
-
-		describe('to the left', () => {
-
-			test('returns element four for element seven', () => {
-
-				const elements = [...simplegrid3by3]
-				const [elementSeven] = elements.splice(6, 1);
-
-				const element = findClosestElement(elementSeven, elements, Direction.WEST, Strategy.STRICT)
-				expect(element.id).toEqual('four')
-
-			})
-		})
-
-		describe('below', () => {
-
-			test('returns element five for element four', () => {
-
-				const elements = [...simplegrid3by3]
-				const [elementFour] = elements.splice(3, 1);
-
-				const element = findClosestElement(elementFour, elements, Direction.SOUTH, Strategy.STRICT)
-				expect(element.id).toEqual('five')
-
-			})
-		})
-
-		describe('above', () => {
-
+		describe('north', () => {
 			test('returns element eight for element nine', () => {
-
 				const elements = [...simplegrid3by3]
 				const [elementNine] = elements.splice(8, 1);
 
 				const element = findClosestElement(elementNine, elements, Direction.NORTH, Strategy.STRICT)
 				expect(element.id).toEqual('eight')
+			})
+		})
 
+
+
+		// TODO should test FLOW strategy separately
+		describe('northeast', () => {
+			test.only('returns element seven for element five', () => {
+				const elements = [...simplegrid3by3]
+				const [elementFive] = elements.splice(4, 1);
+
+				const element = findClosestElement(elementFive, elements, Direction.NORTHEAST, Strategy.STRICT)
+				expect(element.id).toEqual('seven')
+			})
+		})
+
+		describe('east', () => {
+			test('returns element five for element two', () => {
+				const elements = [...simplegrid3by3]
+				const [elementTwo] = elements.splice(1, 1);
+
+				const element = findClosestElement(elementTwo, elements, Direction.EAST, Strategy.STRICT)
+				expect(element.id).toEqual('five')
+			})
+		})
+
+		describe('south', () => {
+			test('returns element five for element four', () => {
+				const elements = [...simplegrid3by3]
+				const [elementFour] = elements.splice(3, 1);
+
+				const element = findClosestElement(elementFour, elements, Direction.SOUTH, Strategy.STRICT)
+				expect(element.id).toEqual('five')
+			})
+		})
+
+		describe('west', () => {
+			test('returns element four for element seven', () => {
+				const elements = [...simplegrid3by3]
+				const [elementSeven] = elements.splice(6, 1);
+
+				const element = findClosestElement(elementSeven, elements, Direction.WEST, Strategy.STRICT)
+				expect(element.id).toEqual('four')
 			})
 		})
 
@@ -63,36 +64,28 @@ describe('findClosestElement', () => {
 
 	describe('unevengrid3by3', () => {
 
-		describe('to the right', () => {
+		describe('north', () => {
+			test('returns element seven for element eight', () => {
+				const elements = [...unevenGrid3by3]
+				const [elementEight] = elements.splice(7, 1);
 
+				const element = findClosestElement(elementEight, elements, Direction.NORTH, Strategy.STRICT)
+				expect(element.id).toEqual('seven')
+			})
+		})
+
+		describe('east', () => {
 			test('returns element five for element two', () => {
-
 				const elements = [...unevenGrid3by3]
 				const [elementTwo] = elements.splice(1, 1);
 
 				const element = findClosestElement(elementTwo, elements, Direction.EAST, Strategy.STRICT)
 				expect(element.id).toEqual('five')
-
 			})
 		})
 
-		describe('to the left', () => {
-
-			test('returns element four for element seven', () => {
-
-				const elements = [...unevenGrid3by3]
-				const [elementSeven] = elements.splice(6, 1);
-
-				const element = findClosestElement(elementSeven, elements, Direction.WEST, Strategy.STRICT)
-				expect(element.id).toEqual('four')
-
-			})
-		})
-
-		describe('below', () => {
-
+		describe('south', () => {
 			test('returns element three for element four', () => {
-
 				const elements = [...unevenGrid3by3]
 				const [elementFour] = elements.splice(3, 1);
 
@@ -102,16 +95,13 @@ describe('findClosestElement', () => {
 			})
 		})
 
-		describe('above', () => {
-
-			test('returns element seven for element eight', () => {
-
+		describe('west', () => {
+			test('returns element four for element seven', () => {
 				const elements = [...unevenGrid3by3]
-				const [elementEight] = elements.splice(7, 1);
+				const [elementSeven] = elements.splice(6, 1);
 
-				const element = findClosestElement(elementEight, elements, Direction.NORTH, Strategy.STRICT)
-				expect(element.id).toEqual('seven')
-
+				const element = findClosestElement(elementSeven, elements, Direction.WEST, Strategy.STRICT)
+				expect(element.id).toEqual('four')
 			})
 		})
 
