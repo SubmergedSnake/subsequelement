2f64b27e67579e00f1713f5a9d5c6297b9e7d692
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-08-01 08:00:45 +0300

    Add finding functions for remaining compass points

--- src/finding/findByDirection.ts
+++ src/finding/findByDirection.ts
@@ -1,19 +1,42 @@
 import { ImplementsGetBoundingClientRect } from "../types"
 
+export const findNorth = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
+	return otherElements.filter(oe => oe.getBoundingClientRect().bottom <= startingElement.getBoundingClientRect().top)
+}
+
+export const findNorthEast = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
+	return otherElements.filter(oe => oe.getBoundingClientRect().bottom <= startingElement.getBoundingClientRect().top
+		&& oe.getBoundingClientRect().left >= startingElement.getBoundingClientRect().right)
+}
+
 export const findEast = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
 	return otherElements.filter(oe => oe.getBoundingClientRect().left >= startingElement.getBoundingClientRect().right)
 }
 
-export const findWest = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
-	return otherElements.filter(oe => oe.getBoundingClientRect().right <= startingElement.getBoundingClientRect().left)
+export const findSouthEast = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
+	return otherElements.filter(oe => oe.getBoundingClientRect().left >= startingElement.getBoundingClientRect().right &&
+		oe.getBoundingClientRect().top >= startingElement.getBoundingClientRect().bottom)
 }
 
 export const findSouth = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
 	return otherElements.filter(oe => oe.getBoundingClientRect().top >= startingElement.getBoundingClientRect().bottom)
 }
 
-export const findNorth = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
-	return otherElements.filter(oe => oe.getBoundingClientRect().bottom <= startingElement.getBoundingClientRect().top)
+export const findSouthWest = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
+	return otherElements.filter(oe => oe.getBoundingClientRect().top >= startingElement.getBoundingClientRect().bottom &&
+		oe.getBoundingClientRect().right <= startingElement.getBoundingClientRect().left)
 }
+
+export const findWest = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
+	return otherElements.filter(oe => oe.getBoundingClientRect().right <= startingElement.getBoundingClientRect().left)
+}
+
+export const findNorthWest = (startingElement: ImplementsGetBoundingClientRect, otherElements: ImplementsGetBoundingClientRect[]): ImplementsGetBoundingClientRect[] => {
+	return otherElements.filter(oe => oe.getBoundingClientRect().right <= startingElement.getBoundingClientRect().left &&
+		oe.getBoundingClientRect().bottom <= startingElement.getBoundingClientRect().top
+	)
+}
+
+
 
 
--- tests/unit/finding/findByDirection.test.ts
+++ tests/unit/finding/findByDirection.test.ts
@@ -1,4 +1,4 @@
-import { findEast, findWest, findNorth, findSouth } from "../../../src/finding/findByDirection"
+import { findEast, findWest, findNorth, findSouth, findNorthEast, findSouthEast, findSouthWest, findNorthWest } from "../../../src/finding/findByDirection"
 import { simplegrid3by3 } from "../mockLayouts/simpleGrid3by3"
 import { unevenGrid3by3 } from "../mockLayouts/unevenGrid3by3"
 
@@ -6,27 +6,50 @@ describe('findByDirection', () => {
 
 	describe('simplegrid3by3', () => {
 		describe('North', () => {
-			test('returns only elements above element five', () => {
+			test('returns only elements north of element five', () => {
 				const [one, two, three, four, five, ...rest] = simplegrid3by3
 				const elements = findNorth(five, [one, two, three, four, ...rest])
 				expect(elements.map(e => e.id).sort()).toEqual(['one', 'four', 'seven'].sort())
 			})
 		})
 
+		describe('NorthEast', () => {
+			test('returns only elements northeast of element five', () => {
+				const [one, two, three, four, five, ...rest] = simplegrid3by3
+				const elements = findNorthEast(five, [one, two, three, four, ...rest])
+				expect(elements.map(e => e.id).sort()).toEqual(['seven'].sort())
+			})
+		})
 
 		describe('East', () => {
-			test('returns only elements to the right of element one', () => {
+			test('returns only elements east of element one', () => {
 				const [one, ...rest] = simplegrid3by3
 				const elements = findEast(one, rest)
 				expect(elements.map(e => e.id).sort()).toEqual(['four', 'five', 'six', 'seven', 'eight', 'nine'].sort())
 			})
 		})
 
+		describe('SouthEast', () => {
+			test('returns only elements southeast of element one', () => {
+				const [one, ...rest] = simplegrid3by3
+				const elements = findSouthEast(one, rest)
+				expect(elements.map(e => e.id).sort()).toEqual(['five', 'six', 'eight', 'nine'].sort())
+			})
+		})
+
 		describe('South', () => {
-			test('returns only elements below element four', () => {
+			test('returns only elements south of element four', () => {
 				const [one, two, three, four, ...rest] = simplegrid3by3
 				const elements = findSouth(four, [one, two, three, ...rest])
 				expect(elements.map(e => e.id).sort()).toEqual(['two', 'three', 'five', 'six', 'eight', 'nine'].sort())
+			})
+		})
+
+		describe('SouthWest', () => {
+			test('returns only elements southwest of element four', () => {
+				const [one, two, three, four, ...rest] = simplegrid3by3
+				const elements = findSouthWest(four, [one, two, three, ...rest])
+				expect(elements.map(e => e.id).sort()).toEqual(['two', 'three'].sort())
 			})
 		})
 
@@ -35,6 +58,14 @@ describe('findByDirection', () => {
 				const [one, two, three, four, ...rest] = simplegrid3by3
 				const elements = findWest(four, [one, two, three, ...rest])
 				expect(elements.map(e => e.id).sort()).toEqual(['one', 'two', 'three'].sort())
+			})
+		})
+
+		describe('NorthWest', () => {
+			test('returns only elements northwest of element five', () => {
+				const [one, two, three, four, five, ...rest] = simplegrid3by3
+				const elements = findNorthWest(five, [one, two, three, four, ...rest])
+				expect(elements.map(e => e.id).sort()).toEqual(['one'].sort())
 			})
 		})
 
