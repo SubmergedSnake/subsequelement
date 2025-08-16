ade4f468035acdf0bfd928202136a4375e847153
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-08-16 15:06:19 +0300

    Findbydirection tests fixed

--- tests/unit/finding/findByDirection.test.ts
+++ tests/unit/finding/findByDirection.test.ts
@@ -1,8 +1,8 @@
 import { beforeEach } from "node:test"
-import { indexPageLayout } from "../../../indexPageLayout"
 import { findInDirection } from "../../../src/finding/findByDirection"
 import { Direction, HasIdAndElementCoords } from "../../../src/types"
 import { unevenGrid3by3 } from "../mockLayouts/unevenGrid3by3"
+import { indexPageLayout } from "../realLayouts/indexPageLayout"
 
 
 const getIndexPageElements = () => [...indexPageLayout]
@@ -25,7 +25,7 @@ describe('findByDirection', () => {
 			expect(elements.map(e => e.id).sort()).toEqual(['C'].sort())
 		})
 
-		test('returns only elements east of element one', () => {
+		test('returns only elements east of element A', () => {
 			const [A] = getIndexPageElements().splice(0, 1);
 			const elements = findInDirection(A, indexPageLayout, Direction.EAST)
 			expect(elements.map(e => e.id).sort()).toEqual(
@@ -39,28 +39,29 @@ describe('findByDirection', () => {
 			expect(elements.map(e => e.id).sort()).toEqual(['Q', 'R', 'T'].sort())
 		})
 
-		test('returns only elements south of element four', () => {
-			const [twelve] = getIndexPageElements().splice(11, 1);
-			const elements = findInDirection(twelve, indexPageLayout, Direction.SOUTH)
-			expect(elements.map(e => e.id).sort()).toEqual(['fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twentyone'].sort())
+		test('returns only elements south of element M', () => {
+			const [M] = getIndexPageElements().splice(13, 1);
+			console.log(M)
+			const elements = findInDirection(M, indexPageLayout, Direction.SOUTH)
+			expect(elements.map(e => e.id).sort()).toEqual(['P', 'Q', 'R', 'S', 'T'].sort())
 		})
 
-		test('returns only elements southwest of element fifteen', () => {
-			const [fifteen] = getIndexPageElements().splice(14, 1);
-			const elements = findInDirection(fifteen, indexPageLayout, Direction.SOUTHWEST)
-			expect(elements.map(e => e.id).sort()).toEqual(['seventeen', 'twenty'].sort())
+		test('returns only elements southwest of element M', () => {
+			const [M] = getIndexPageElements().splice(13, 1);
+			const elements = findInDirection(M, indexPageLayout, Direction.SOUTHWEST)
+			expect(elements.map(e => e.id).sort()).toEqual(['P', 'S'].sort())
 		})
 
-		test('returns only elements to the left of element four', () => {
-			const [one, two, three, four, ...rest] = indexPageLayout
-			const elements = findInDirection(four, [one, two, three, ...rest], Direction.WEST)
-			expect(elements.map(e => e.id).sort()).toEqual(['one', 'two', 'three'].sort())
+		test('returns only elements to the west of element B', () => {
+			const [B] = getIndexPageElements().splice(1, 1);
+			const elements = findInDirection(B, indexPageLayout, Direction.WEST)
+			expect(elements.map(e => e.id).sort()).toEqual(['A', 'D', 'G', 'J', 'N', 'P', 'S'].sort())
 		})
 
 		test('returns only elements northwest of element five', () => {
-			const [one, two, three, four, five, ...rest] = indexPageLayout
-			const elements = findInDirection(five, [one, two, three, four, ...rest], Direction.NORTHWEST)
-			expect(elements.map(e => e.id).sort()).toEqual(['one'].sort())
+			const [H] = getIndexPageElements().splice(7, 1);
+			const elements = findInDirection(H, indexPageLayout, Direction.NORTHWEST)
+			expect(elements.map(e => e.id).sort()).toEqual(['D', 'A'].sort())
 		})
 
 	})
