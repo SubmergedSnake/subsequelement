33b3ced318cbb468b51953dd42d01d8c42d16741
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-09-19 23:35:52 +0300

    Use test type for irregulargridtests, too

--- tests/e2e/specs/farthest/irregulargrid.spec.ts
+++ tests/e2e/specs/farthest/irregulargrid.spec.ts
@@ -1,7 +1,8 @@
 import { HasToAlign, Bearing } from '../../../../src/types';
 import { assertFarthestElementIds } from '../../utils/assertFarthestElementIds';
+import { ElementIdTest } from '../testtypes';
 
-const irregularGridTests: { desc: string, startingElementId: string, bearing: keyof typeof Bearing, hasToAlign?: HasToAlign, expectedId: string }[] = [
+const irregularGridTests: ElementIdTest[] = [
   { desc: 'farthest n(orth)w(est) of D is A (irregulargrid)', startingElementId: 'D', bearing: 'nw', hasToAlign: HasToAlign.ASMUCHASPOSSIBLE, expectedId: 'A' },
   { desc: 'absolute farthest e(ast) of A is E (irregulargrid)', startingElementId: 'A', bearing: 'e', hasToAlign: HasToAlign.NO, expectedId: 'E' },
 ]
